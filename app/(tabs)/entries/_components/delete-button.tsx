17d582f248249729f305171fceea768432e8a623
marginalfriend <abdullahpsg@gmail.com>
2024-09-22 11:49:02 +0700

    [NEW] alert dialog before deleting entry

--- app/(tabs)/entries/_components/delete-button.tsx
+++ app/(tabs)/entries/_components/delete-button.tsx
@@ -6,6 +6,17 @@ import { Button } from "@/components/ui/button";
 import { useAuthFetch } from "@/hooks/use-auth-fetch";
 import { useToast } from "@/hooks/use-toast";
 import { useEntry } from "@/hooks/use-entry";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const DeleteButton = ({
   id,
@@ -41,9 +52,26 @@ const DeleteButton = ({
   };
 
   return (
-    <Button className="w-9 p-2" variant={"destructive"} onClick={eraseEntry}>
-      <Trash2 className="w-4 h-4" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="w-9 p-2" variant={"destructive"}>
+          <Trash2 className="w-4 h-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the entry
+            from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={eraseEntry}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
