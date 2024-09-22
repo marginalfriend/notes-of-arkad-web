fb0b711fb4c31ff58fd327e024bf034852f25f2d
marginalfriend <abdullahpsg@gmail.com>
2024-09-22 15:35:33 +0700

    [FIX] client side fetching

--- contexts/entry-context.tsx
+++ contexts/entry-context.tsx
@@ -4,63 +4,128 @@ import { Entry } from "@/app/(tabs)/entries/_components/columns";
 import { useAuthFetch } from "@/hooks/use-auth-fetch";
 import { useToast } from "@/hooks/use-toast";
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { useRefetch } from "./refetch-context";
 
-export const EntryContext = createContext({
-  entries: [] as Entry[],
-  addEntry: (entry: Entry) => {},
-  updateEntry: (entry: Entry) => {},
-  deleteEntry: (id: string) => {},
-  isLoading: false,
-});
+type SummaryData = {
+  income: {
+    total: number;
+    percentage: number | undefined;
+  };
+  expense: {
+    total: number;
+    percentage: number | undefined;
+  };
+  saving: {
+    total: number;
+    percentage: number | undefined;
+  };
+};
+
+const defaultSummaryData: SummaryData = {
+  income: {
+    total: 0, // Default total for income
+    percentage: undefined, // Default undefined percentage
+  },
+  expense: {
+    total: 0, // Default total for expense
+    percentage: undefined, // Default undefined percentage
+  },
+  saving: {
+    total: 0, // Default total for saving
+    percentage: undefined, // Default undefined percentage
+  },
+};
+
+export type EntryContextProps = {
+  summary: SummaryData;
+  entries: Entry[];
+  addEntry: (entry: Entry) => void;
+  updateEntry: (entry: Entry) => void;
+  deleteEntry: (id: string) => void;
+  isLoading: { summary: boolean; entry: boolean };
+};
+
+export const EntryContext = createContext<EntryContextProps | undefined>(
+  undefined
+);
 
 export const EntryProvider = ({ children }: { children: React.ReactNode }) => {
   const [entries, setEntries] = useState<Entry[]>([]);
+  const [summary, setSummary] = useState<SummaryData>(defaultSummaryData);
+  const [isLoading, setIsLoading] = useState({ entry: true, summary: true });
   const authFetch = useAuthFetch();
-  const [isLoading, setIsLoading] = useState(true);
   const { toast } = useToast();
+
+  const handleError = (error: any) => {
+    console.error(error);
+    toast({
+      title: "Failed to retrieve entry data",
+      description:
+        "Something went wrong while retrieving data, try refresh the page.",
+    });
+  };
 
   const addEntry = (entry: Entry) => {
     setEntries([entry, ...entries]);
+    fetchSummary();
   };
 
   const deleteEntry = (id: string) => {
     const updated = entries.filter((e) => e.id !== id);
     setEntries(updated);
+    fetchSummary();
   };
 
   const updateEntry = (entry: Entry) => {
     const updated = entries.filter((e) => e.id !== entry.id);
     updated.push(entry);
     setEntries(updated);
+    fetchSummary();
   };
 
-  const fetchEntries = async () => {
+  const fetchEntries = () => {
     try {
       authFetch(`/api/entry`, {
         next: { tags: ["entries"] },
       })
         .then((res) => res.json())
-        .then((unwrapped) => setEntries(unwrapped.entries))
-        .then(() => setIsLoading(false));
+        .then((data) => setEntries(data.entries))
+        .then(() => {
+          setIsLoading({ summary: isLoading.summary, entry: false });
+        });
     } catch (error) {
-      console.error(error);
-      toast({
-        title: "Failed to retrieve entry data",
-        description:
-          "Something went wrong when retrieving entries data, try refresh the page.",
-      });
+      handleError(error);
+    }
+  };
+
+  const fetchSummary = () => {
+    try {
+      authFetch("/api/summary")
+        .then((res: Response) => res.json())
+        .then((data: any) => setSummary(data.data))
+        .then(() => {
+          setIsLoading({ summary: false, entry: isLoading.entry });
+        });
+    } catch (error) {
+      handleError(error);
     }
   };
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchEntries().then(() => setIsLoading(false));
+    setIsLoading({ entry: true, summary: true });
+    fetchEntries();
+    fetchSummary();
   }, []);
 
   return (
     <EntryContext.Provider
-      value={{ entries, addEntry, deleteEntry, updateEntry, isLoading }}
+      value={{
+        entries,
+        addEntry,
+        deleteEntry,
+        updateEntry,
+        isLoading,
+        summary,
+      }}
     >
       {children}
     </EntryContext.Provider>
